539e50eca2488fb4e03cdad211a0dcddeed39634
tpodolskiy <tpodolskiyqa@gmail.com>
2021-01-03 21:21:43 -0800

    POM refactoring

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const selenium = require("selenium-webdriver"); // init the selenium
 const By = selenium.By; // importing By. module
 
 // importing home.js for POM
-const HomePage = require('./pages/home').default;
+const HomePage = require('pages/home.js').default;
 
 
 
@@ -12,7 +12,9 @@ const driver = new selenium.Builder() // building the selenium
     .build(); // building
 
 
-const HomePage = new HomePage(driver);
+const homePage = new HomePage(driver);
+homePage.open();
+
 
 
 
@@ -44,35 +46,10 @@ const invitees = [
 ];
 
 // locators object
-const locators = {
-    inviteeForm: By.id("registrar"),
-    inviteeNameField: By.css("#registrar input[name='name'"),
-    toggleNonRespondersVisibility: By.css(".main > div input"),
-    removeButtonForInvitee: invitee => By.xpath(`//span[text() = "${invitee}"]/../button[last()]`)
-};
 
 // locate and add invitee function
-function addInvitee(name) {
-    driver.findElement(locators.inviteeNameField)
-        .sendKeys(name);
-
-    driver.findElement(locators.inviteeForm)
-        .submit();
-}
-
-
-// remove invitee function
-function removeInvitee(invitee) {
-    driver.findElement(locators.removeButtonForInvitee(invitee))
-        .click();
-}
-// clicking the checkbox to hide/show invitee's
-function toggleNonRespondersVisibility() {
-    driver.findElement(locators.toggleNonRespondersVisibility)
-        .click();
-}
 
 // looping through the array of invitees and calling the addInvitee() function
-invitees.forEach(addInvitee);
-toggleNonRespondersVisibility();
-removeInvitee("Shadd Anderson");
+invitees.forEach(homePage.addInvitee, homePage);
+homePage.removeInvitee("Shadd Anderson");
+homePage.toggleNonRespondersVisibility();
--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,40 @@
+const By = require("selenium-webdriver").By;
+
 class HomePage {
     constructor(driver) {
         this.driver = driver;
+        this.locators = {
+            inviteeForm: By.id("registrar"),
+            inviteeNameField: By.css("#registrar input[name='name'"),
+            toggleNonRespondersVisibility: By.css(".main > div input"),
+            removeButtonForInvitee: invitee => By.xpath(`//span[text() = "${invitee}"]/../button[last()]`)
+        };
+
     }
     open() {
         this.driver.get(process.env.URL); // will be pasting a link when calling node from terminal
     }
+
+    addInvitee(name) {
+        this.driver.findElement(this.locators.inviteeNameField)
+            .sendKeys(name);
+
+        this.driver.findElement(this.locators.inviteeForm)
+            .submit();
+    }
+
+
+    // remove invitee function
+    removeInvitee(invitee) {
+        this.driver.findElement(this.locators.removeButtonForInvitee(invitee))
+            .click();
+    }
+    // clicking the checkbox to hide/show invitee's
+    toggleNonRespondersVisibility() {
+        this.driver.findElement(this.locators.toggleNonRespondersVisibility)
+            .click();
+    }
+
 }
 
-export default HomePage;
+exports = HomePage;
